a40cc7d613edf1ab86305534e11782065c545bdf
Joyce Lee <81258562+joycerlz@users.noreply.github.com>
2022-10-13 01:01:07 -0700

    Add files via upload

---
+++ password_manager/password_manager.py
@@ -0,0 +1,47 @@
+from curses import keyname
+from cryptography.fernet import Fernet  #module
+
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+master_pwd = input("What is the master password? ")
+key = load_key() + master_pwd.encode()  # encode -> takes string and turn it into bytes (to combine with key)
+fer = Fernet(key)
+
+def view():
+    with open('passwords.txt', 'r') as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, passw = data.split("|")
+            print("User: " + user + ", Password: " + fer.decrypt(passw.encode()).decode())
+
+def add():
+    name = input("Account Name: ")
+    pwd = input("Password: ")
+
+    with open('passwords.txt', 'a') as f:
+        f.write(name + "|" + fer.encrypt(pwd.encode()).decode() + "\n")
+
+while True:
+    mode = input("Would you like to add a new password or view existing ones? (view/add) q to quit ")
+
+    if mode == "q":
+        break
+
+    if mode == "view":
+        view()
+    elif mode == "add":
+        add()
+    else:
+        print("Invalid input")
+        continue
+
+'''
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+'''
